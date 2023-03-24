86209b341f415f3b0d7e3f3b8ff8453ada590096
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2023-03-24 15:50:33 +0100

    Remove cross-compilation patch
    Use -DDB_CLIENT_RUN=0 instead

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,18 @@
+if [[ "$host_alias" != "$build_alias" ]]
+then
+  # We cannot use try_run when cross compiling
+  ADDITIONAL_ARGS="-DDB_CLIENT_RUN=0"
+else
+  ADDITIONAL_ARGS=""
+fi
+
 mkdir build
 cd build
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_VERBOSE_MAKEFILE=true \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      ..
+      ${ADDITIONAL_ARGS} ..
 
 make -j $CPU_COUNT
 make install
